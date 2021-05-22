ddb5be4c501aee12f8c41fe6c082a20dc85b3f84
Mark Boyle <markboyle19@gmail.com>
2021-05-22 18:31:23 +1000

    Fixed styling issue of the layout on home and show pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,9 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = Product.all
+    @products = Product.all.includes(:user).with_attached_picture
   end
 
   def edit
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   belongs_to :user
   has_one_attached :picture
   has_many :orders
+  validates :name, :price, :location, :description, :size, presence: true
 end
